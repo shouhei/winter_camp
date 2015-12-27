a7ba952ddea48c649d49942e120d8f32b5427172
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-12-27 15:00:24 +0900

    update todo

--- SplitFileClassTodo/Todo.py
+++ SplitFileClassTodo/Todo.py
@@ -4,14 +4,22 @@ import os
 
 class Todo:
 
-    def __init__(self):
-        self.todo = []
+    def __init__(self, list_name="sample"):
+        self.__list_name = list_name
+        self.__todo = []
+
+    def get_list_name(self):
+        return self.__list_name
 
     def add(self, todo):
-        self.todo.append(todo)
+        self.__todo.append(todo)
 
-    def read(self):
-        return self.todo
+    def get_all(self):
+        return self.__todo
+
+    def get_at(self,index):
+        if len(self.__todo):
+            return self.__todo[index]
 
     def remove(self, todo_index):
-        del self.todo[todo_index]
+        del self.__todo[todo_index]
--- SplitFileClassTodo/test/test_Todo.py
+++ SplitFileClassTodo/test/test_Todo.py
@@ -3,19 +3,34 @@ from unittest import TestCase
 
 class TodoTestCase(TestCase):
 
-    def test_read(self):
-        todo = Todo()
-        self.assertListEqual(todo.read(), [])
+    def test_get_all(self):
+        todo = Todo("test")
+        self.assertListEqual(todo.get_all(), [])
 
     def test_add(self):
-        todo = Todo()
+        todo = Todo("test")
         todo.add("hoge")
-        self.assertListEqual(todo.read(), ["hoge"])
+        self.assertListEqual(todo.get_all(), ["hoge"])
 
     def test_remove(self):
-        todo = Todo()
+        todo = Todo("test")
         todo.add("hoge")
         todo.add("fuga")
         todo.add("piyo")
         todo.remove(1)
-        self.assertListEqual(todo.read(), ["hoge", "piyo"])
+        self.assertListEqual(todo.get_all(), ["hoge", "piyo"])
+
+    def test_get_at(self):
+        todo = Todo("test")
+        todo.add("hoge")
+        todo.add("fuga")
+        todo.add("piyo")
+        self.assertEqual(todo.get_at(0), "hoge")
+
+    def test_get_list_name(self):
+        todo = Todo("test")
+        self.assertEqual(todo.get_list_name(), "test")
+
+    def test_get_list_name_default(self):
+        todo = Todo()
+        self.assertEqual(todo.get_list_name(), "sample")
